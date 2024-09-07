5ab001ee3b688601d82596d3707d86fa81eadc2c
unknown <ceedriclazaro010205@gmail.com>
2024-09-06 22:17:34 -0800

    Initial commit with app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const favNumber1 = 10;
+const favNumber2 = 69;
+const sum = favNumber1 + favNumber2;
+console.log(sum);
+
+const favNumber3 = 12;
+const favNumber4 = 32;
+const difference = favNumber3 - favNumber4;
+console.log(difference);
+
+const favNumber5 = 18;
+const favNumber6 = 47;
+const product = favNumber5 * favNumber6;
+console.log(product);
+
+const favNumber7 = 8;
+const favNumber8 = 24;
+const qoutient = favNumber1 / favNumber2;
+console.log(qoutient); 
